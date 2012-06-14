2b885c2f178de1c7568241755f120e105df543df
Kobaj.G@gmail.com <Kobaj.G@gmail.com@6e2a81f4-53be-256d-0ce4-2b7c6d73cb0f>
2012-06-14 03:55:40 +0000

    finishing up lighting effects tests. Need to put them into classes now, figure out translation and rotation of said lights. Then move onto the rest of my list of things to do.

--- src/com/kobaj/activity/SurfacePanel.java
+++ src/com/kobaj/activity/SurfacePanel.java
@@ -3,19 +3,25 @@ package com.kobaj.activity;
 import com.kobaj.foxdashtwo.R;
 
 import android.content.Context;
-import android.graphics.AvoidXfermode;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapShader;
-import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.ComposeShader;
+import android.graphics.LightingColorFilter;
 import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.PorterDuffXfermode;
-import android.graphics.BlurMaskFilter.Blur;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.Paint.Style;
 import android.graphics.Shader.TileMode;
 
 //surface class that updates and draws everything.
@@ -37,6 +43,8 @@ public class SurfacePanel extends DrawablePanel
 	
 	Paint redP, greenP, blueP;
 	
+	Paint outline;
+	
 	// load in our resources
 	public void onInitialize()
 	{
@@ -44,84 +52,27 @@ public class SurfacePanel extends DrawablePanel
 	    opt.inPreferredConfig = Config.ARGB_8888;
 		bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.titlescreen, opt);
 		paint = new Paint();
-		BitmapShader shady = new BitmapShader(bitmap, TileMode.CLAMP, TileMode.CLAMP);
+		ColorFilter filter = new LightingColorFilter(Color.argb(255, 100, 100, 100), 1);
+		paint.setColorFilter(filter);
 		
-		paint.setShader(shady);
-		
-		blur = new Paint();
-		//blur.setAntiAlias(true);
-		//blur.setMaskFilter(new BlurMaskFilter(25, Blur.NORMAL));
-		LinearGradient shader = new LinearGradient(
-	            0, 
-	            0,
-	            0,
-	            200,
-	            0xFF00FF00, 0x00000000, TileMode.CLAMP);
-		
-		blur.setShader(shader);
-	    blur.setStyle(Paint.Style.FILL);
-	    //lighten acts like alight
-	    //multiply changes color
-		blur.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.LIGHTEN));
-	    
-	    gradient = new Paint();
-	    gradient.setShader(shader);
-	    gradient.setStyle(Paint.Style.FILL);
-	
-	    red_light = new Paint();
-	    red_light.setColor(Color.RED);
-	    red_light.setStyle(Paint.Style.FILL);
-	    red_light.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.LIGHTEN));
-	    
-	    blue_light = new Paint();
-	    blue_light.setColor(Color.RED);
-		LinearGradient shader2 = new LinearGradient(
-	            0, 
-	            0,
-	            0,
-	            200,
-	            0xFF0000FF, 0x00000000, TileMode.CLAMP);
-		blue_light.setShader(shader2);
-	    blue_light.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.LIGHTEN));
-	    
-	    ambient = new Paint();
-	    ambient.setColor(Color.BLACK);
-	    ambient.setStyle(Paint.Style.FILL);
-	    ambient.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.LIGHTEN));
-	    
-	    
-	    //new try
-	    LinearGradient shader3 = new LinearGradient(
-	            0, 
-	            0,
-	            0,
-	            200,
-	            0xFFFFFFFF, 0xFFFFFFFF, TileMode.CLAMP);
-	    
-	    redP = new Paint();
-	    redP.setShader(shader3);
-	    redP.setColorFilter(new PorterDuffColorFilter(Color.RED, android.graphics.PorterDuff.Mode.MULTIPLY));
-	    //use this for a bright intense light
-	    //redP.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.SCREEN));
-	    //use this for a subtle color change
-	    redP.setXfermode(new AvoidXfermode(Color.RED, 255, AvoidXfermode.Mode.TARGET));
-	    
-	    greenP = new Paint();
-	    greenP.setShader(shader3);
-	    greenP.setColorFilter(new PorterDuffColorFilter(Color.GREEN,android.graphics.PorterDuff.Mode.MULTIPLY));
-	    //greenP.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.SCREEN));
-	    greenP.setXfermode(new AvoidXfermode(Color.GREEN, 255, AvoidXfermode.Mode.TARGET));
-	 
 	    blueP = new Paint();
-	    blueP.setShader(shader3);
-	    blueP.setColorFilter(new PorterDuffColorFilter(Color.BLUE,android.graphics.PorterDuff.Mode.MULTIPLY));
-	    //blueP.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.SCREEN));
-	    blueP.setXfermode(new AvoidXfermode(Color.BLUE, 255, AvoidXfermode.Mode.TARGET));
+	    //blueP.setColorFilter(new PorterDuffColorFilter(Color.BLUE,android.graphics.PorterDuff.Mode.MULTIPLY));
+	    blueP.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.SRC_IN));
+	    
+		RadialGradient shader4 = new RadialGradient(240, 400, 250, Color.WHITE, Color.TRANSPARENT, Shader.TileMode.CLAMP);
+	    outline = new Paint();
+	    outline.setShader(shader4);
 	}
+	
+	long change;
+	long last;
 	
 	//run logic
 	public void onUpdate(long gameTime)
 	{
+		change = gameTime -last;
+		last = gameTime;
+		
 		
 	}
 	
@@ -129,17 +80,52 @@ public class SurfacePanel extends DrawablePanel
 	public void onDraw(Canvas canvas)
 	{
 		super.onDraw(canvas);
+		float w = 480;
+		float h = 800;
 		
-		//canvas.drawBitmap(bitmap, 0, 0, paint);
-		canvas.drawRect(0,0,bitmap.getWidth(), bitmap.getHeight(), paint);
+		//ambient base layer
+		canvas.drawBitmap(bitmap, 0, 0, paint);//canvas.drawRect(0,0,bitmap.getWidth(), bitmap.getHeight(), paint);
 		
-		//ambient
-		//canvas.drawRect(0,0,400,400, ambient);
+		//move the bitmap offscreen
+		int sc = canvas.saveLayer(0, 0, w, h, null,
+                Canvas.MATRIX_SAVE_FLAG |
+                Canvas.CLIP_SAVE_FLAG |
+                Canvas.HAS_ALPHA_LAYER_SAVE_FLAG |
+                Canvas.FULL_COLOR_LAYER_SAVE_FLAG |
+                Canvas.CLIP_TO_LAYER_SAVE_FLAG);
+		
+		//canvas.translate(w / 2.0f, 0);
+		
+		//obscuring light
+		outline.setXfermode(null);
+		outline.setAlpha(255);
+		Path pth = new Path();
+	    pth.moveTo(w*0.27f,0);
+	    pth.lineTo(w*0.73f,0);
+	    pth.lineTo(w*0.92f,h);
+	    pth.lineTo(w*0.08f,h);
+	    pth.lineTo(w*0.27f,0);
+	    canvas.drawPath(pth,outline);
 		
 		//light
-		canvas.drawRect(0, 0, 200, 250, redP);
-		canvas.drawRect(100,0, 300, 250, greenP);
-		canvas.drawRect(175,0, 230, 250, blueP);
+		canvas.drawBitmap(bitmap, 0 , 0, blueP);
+		
+		//bloom
+		outline.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.SRC_IN));
+		outline.setAlpha(90);
+		canvas.drawPath(pth, outline);
+		
+	    
+	    //put the scene back
+	    canvas.restoreToCount(sc);
+
+	    //little bit of after effect
+	    
+		
+		//light
+		//canvas.drawRect(0, 0, 200, 250,	redP);
+		//canvas.drawRect(100,0, 300, 250, greenP);
+		//canvas.drawRect(175,0, 230, 250, blueP);
 		
 		//regular square
 		canvas.drawRect(305, 0, 480, 200, gradient);
