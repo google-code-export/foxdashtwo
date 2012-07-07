43a21382fa654cc58fa3178308c031cd3d1d710a
Kobaj.G@gmail.com <Kobaj.G@gmail.com@6e2a81f4-53be-256d-0ce4-2b7c6d73cb0f>
2012-07-07 05:00:34 +0000

    Putting in placeholder methods that I need to fully implement soon.

---
+++ trunk/src/com/kobaj/opengldrawable/Particles.java
@@ -0,0 +1,9 @@
+package com.kobaj.opengldrawable;
+
+public class Particles
+{
+	//hells yeah particles
+	//this will start off just as a special collection of quads
+	//changing shape, size, and color
+	//but might evolve into a system using its own shader
+}
