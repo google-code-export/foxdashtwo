63bd88e8513eb46e94dce979aec9dfb8afa56066
Kobaj.G@gmail.com <Kobaj.G@gmail.com@6e2a81f4-53be-256d-0ce4-2b7c6d73cb0f>
2012-10-22 00:50:19 +0000

    Restructuring of file system for Unit Testing

---
+++ trunk/foxdashtwo/src/com/kobaj/opengl/MyGame.java
@@ -0,0 +1,112 @@
+package com.kobaj.opengl;
+
+import android.graphics.Color;
+import android.opengl.GLES20;
+
+import com.kobaj.math.Constants;
+import com.kobaj.math.Functions;
+import com.kobaj.opengldrawable.EnumDrawFrom;
+import com.kobaj.opengldrawable.Quad.QuadRenderTo;
+import com.kobaj.screen.BaseScreen;
+import com.kobaj.screen.EnumScreenState;
+import com.kobaj.screen.SinglePlayerScreen;
+
+public class MyGame extends MyGLRender
+{	
+	//test screen
+	private SinglePlayerScreen single_player_screen;
+	private BaseScreen currently_active_screen;
+	
+	//dont touch the stuff below this line
+	//final drawable.
+	private QuadRenderTo scene;
+
+    public MyGame()
+    {
+    	single_player_screen = new SinglePlayerScreen();
+    	currently_active_screen = single_player_screen;
+    }
+    
+	
+	//for the record this is called everytime the screen is reset/paused/resumed
+	//all graphics are destroyed (dunno about sounds >.>).
+	@Override
+	protected void onInitialize()
+	{
+		//begin by aligning our functions
+		Functions.adjustConstantsToScreen();
+		
+		currently_active_screen.onInitialize();
+		
+		//dont touch below this line.
+        scene = new QuadRenderTo();
+        
+        System.gc();
+	}
+	
+	@Override
+	protected void onUpdate(double delta)
+	{
+		if(currently_active_screen.current_state == EnumScreenState.running)
+			currently_active_screen.onUpdate(delta);
+	}
+	
+	@Override
+	protected void onDraw()
+	{	
+		GLES20.glUseProgram(Constants.ambient_light.my_shader);
+		
+		if(currently_active_screen.current_state == EnumScreenState.running)
+			onRunningDraw();
+		else if(currently_active_screen.current_state == EnumScreenState.loading)
+			onLoadingDraw();
+		
+		//fps
+		int color = Color.WHITE;
+		if(fps.fps < 60)
+			color = Color.GREEN;
+		if(fps.fps < 45)
+			color = Color.YELLOW;
+		if(fps.fps < 30)
+			color = Color.RED;
+		
+		Constants.text.drawNumber(fps.fps, Functions.screenXToShaderX(25), Functions.screenYToShaderY((int)Functions.fix_y(25)), EnumDrawFrom.top_left, color);
+	}
+	
+	private void onLoadingDraw()
+	{
+		currently_active_screen.onDrawLoading(fps.getDelta());
+	}
+	
+	private void onRunningDraw()
+	{	
+		//regular objects
+		GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA); // no see thru
+		if(scene.beginRenderToTexture())
+		{	
+			//put opaque items here
+			currently_active_screen.onDrawObject();
+		}
+		scene.endRenderToTexture();
+	
+		//lights
+		GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_SRC_ALPHA); // cheap lights
+
+		//put translucent (lights) here
+		currently_active_screen.onDrawLight();
+		
+		//final scene
+		GLES20.glBlendFunc(GLES20.GL_DST_COLOR, GLES20.GL_ZERO); // masking
+		scene.onDrawAmbient(Constants.identity_matrix, Constants.my_proj_matrix, Color.WHITE, true);
+		
+		//text below this line
+		GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA); // no see thru
+		currently_active_screen.onDrawConstant();	
+	}
+
+	@Override
+	protected void onPause()
+	{
+		
+	}
+}
---
+++ trunk/foxdashtwo/src/com/kobaj/openglgraphics/BaseLightShader.java
@@ -0,0 +1,83 @@
+package com.kobaj.openglgraphics;
+
+import android.opengl.GLES20;
+import android.util.Log;
+
+public abstract class BaseLightShader
+{
+	// shader (point light)
+	final public int my_shader;
+	
+	// handles to everything in the shader
+	final public int my_position_handle; 
+	final public int my_color_handle; 
+	final public int my_tex_coord_handle; 
+	final public int my_texture_uniform_handle;
+	final public int my_mvp_matrix_handle;
+	
+	protected BaseLightShader(int r_vertex_shader, int r_fragment_shader)
+	{
+		// initialize shaders
+		String vertex_shader_code = com.kobaj.loader.RawTextReader.readRawTextFile(com.kobaj.math.Constants.context, r_vertex_shader);
+		String fragment_shader_code = com.kobaj.loader.RawTextReader.readRawTextFile(com.kobaj.math.Constants.context, r_fragment_shader);
+		
+		// shaders
+		int vertex_shader = loadShader(GLES20.GL_VERTEX_SHADER, vertex_shader_code);
+		int fragment_shader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragment_shader_code);
+		
+		my_shader = GLES20.glCreateProgram(); // create empty OpenGL Program
+		GLES20.glAttachShader(my_shader, vertex_shader); 
+		GLES20.glAttachShader(my_shader, fragment_shader); 
+		GLES20.glLinkProgram(my_shader); // creates OpenGL program executables
+		
+		// get handle to the vertex shader's vPosition member
+		my_position_handle = GLES20.glGetAttribLocation(my_shader, "a_Position");
+		my_tex_coord_handle = GLES20.glGetAttribLocation(my_shader, "a_TexCoordinate");
+		
+		my_color_handle = GLES20.glGetUniformLocation(my_shader, "u_Color");
+		
+		my_mvp_matrix_handle = GLES20.glGetUniformLocation(my_shader, "u_MVPMatrix");
+		my_texture_uniform_handle = GLES20.glGetUniformLocation(my_shader, "u_Texture");
+	}
+	
+	private int loadShader(int type, String shaderCode)
+	{
+		int shader = GLES20.glCreateShader(type);
+		
+		if (shader != 0)
+		{
+			// add the source code to the shader and compile it
+			GLES20.glShaderSource(shader, shaderCode);
+			GLES20.glCompileShader(shader);
+			
+			// Get the compilation status.
+			final int[] compileStatus = new int[1];
+			GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+			
+			// If the compilation failed, delete the shader.
+			if (compileStatus[0] == 0)
+			{
+				//small bit of info
+			
+				String type_name = "Vertex Shader";
+				if(type == GLES20.GL_FRAGMENT_SHADER)
+					type_name = "Fragment Shader";
+				
+				//returns absolutely nothing :(.
+				String infoLog = GLES20.glGetShaderInfoLog( shader );
+					
+				Log.e("kobaj_shader_error", "Error compiling shader: " + type_name + " " + infoLog);
+				
+				GLES20.glDeleteShader(shader);
+				shader = 0;
+			}
+		}
+		
+		if (shader == 0)
+		{
+			throw new RuntimeException("Error creating shader.");
+		}
+		
+		return shader;
+	}
+}
