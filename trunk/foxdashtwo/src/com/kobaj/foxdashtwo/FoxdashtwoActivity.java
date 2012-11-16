e8bb090fa29c55b1b099df111809f8d609012542
Kobaj.G@gmail.com <Kobaj.G@gmail.com@6e2a81f4-53be-256d-0ce4-2b7c6d73cb0f>
2012-11-16 01:40:28 +0000

    Finished the basic implementation of popups.

--- trunk/foxdashtwo/src/com/kobaj/foxdashtwo/FoxdashtwoActivity.java
+++ trunk/foxdashtwo/src/com/kobaj/foxdashtwo/FoxdashtwoActivity.java
@@ -1,31 +1,35 @@
 package com.kobaj.foxdashtwo;
 
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.widget.EditText;
 
+import com.kobaj.loader.FileHandler;
+import com.kobaj.message.PopupManager;
 import com.kobaj.screen.SinglePlayerScreen;
-import com.kobaj.message.*;
 
-public final class FoxdashtwoActivity extends GameActivity implements PopupManager.NoticeDialogListener 
+public final class FoxdashtwoActivity extends GameActivity implements PopupManager.NoticeDialogListener
 {
+	// this is awesome http://www.regexplanet.com/advanced/java/index.html
+	private static final String empty = "";
+	private static final String space = " ";
+	private static final String period = ".";
+	private static final String reg_remove_space = "\\s*\\.\\s*";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-	
+		
 		mGLView.my_game.onChangeScreen(new SinglePlayerScreen());
 	}
-
-	public void onDialogPositiveClick(DialogFragment dialog)
+	
+	public void onDialogPositiveClick(PopupManager dialog)
 	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onDialogNegativeClick(DialogFragment dialog)
-	{
-		// TODO Auto-generated method stub
-		
+		EditText view = (EditText) dialog.mView.findViewById(R.id.username_edit);
+		String value = view.getText().toString().trim().replaceAll(reg_remove_space, period);
+		if (value.equals(empty) || value.equals(space) || value.equals(false))
+			return;
+		FileHandler.writeTextFile(SinglePlayerScreen.save_file_name, value);
+		mGLView.my_game.onChangeScreen(new SinglePlayerScreen());
 	}
 }
